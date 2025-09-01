32b184ca14149c7e8b30e600661a9f6408d5b768
stalkrbeardedler18 <rumlerguoyi@hotmail.com>
2025-09-01 10:45:16 -0500

    Refactor MongoDB connection to use async/await for better error handling.

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ app.use(express.json());
 
 // Connect to MongoDB
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/snippets', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
-  .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.log(err));
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/snippets', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err);
+    process.exit(1); // Exit process with failure
+  }
+};
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Simple Code Snippet Manager!');
