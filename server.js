363d5601b7a39fc73d5c437bf20e86d5bb4fc791
stalkrbeardedler18 <rumlerguoyi@hotmail.com>
2025-09-01 10:42:01 -0500

    Updated MongoDB connection string to use environment variable for better configuration management.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/snippets', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/snippets', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
