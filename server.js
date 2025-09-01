88e7e84f7f50484b6b6659c97046ad62bd6316bd
stalkrbeardedler18 <rumlerguoyi@hotmail.com>
2025-09-01 10:40:48 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/snippets', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Simple Code Snippet Manager!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
